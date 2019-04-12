58969e93e0c70f81db5c9ace75a8f2a975e1d621
Lisa ER <lisaerein@gmail.com>
2019-04-12 14:22:59 -0500

    Updated citab function
    fix label order mismatch

--- R/citab.R
+++ R/citab.R
@@ -1,15 +1,15 @@
-#' Lisa's Cumulative Incidence Survival Table 
+#' Lisa's Cumulative Incidence Survival Table
 #'
 #' This function creates a summary table from a survfit object of type "mstate" including cumulative incidence estimates with CIs.
 #' It returns a dataframe and prints an html table by default for use in R markdown documents.
 #' @param msfit survfit object of type "mstate" (REQUIRED).
 #' @param times Numeric vector of times for cumulative incidence estimates (REQUIRED).
 #' @param timelab Character label for time. Default = "Time".
-#' @param events Character vector for event names (including state 0).
-#' @param evlabs Character vector for event labels.
+#' @param events Character vector for event names (only include state 0 if state0 not NA).
+#' @param evlabs Character vector for event labels (only include state 0 if state0 not NA).
 #' @param groups Character vector for groups as listed in dataset. Default = NA (no groups).
 #' @param grlabs Character vector for group names, must be in same order as groups. Default = NA (no groups or use group levels from dataset).
-#' @param state0 Character label for state 0. Default = NA (do not display estimates for state 0).
+#' @param state0 Character event name for state 0. Default = NA (do not display estimates for state 0).
 #' @param ci.dec Number of decimal places to report for cumlative incidence estimates. Default = 2.
 #' @param perc Logical indicator to report estimates as percentages. Default = FALSE.
 #' @param color Character Hex color to use for htmlTable striping. Default = "#EEEEEE" (light grey).
@@ -19,7 +19,7 @@
 #' @importFrom knitr knit_print
 #' @importFrom htmlTable htmlTable
 #' @export
-citab   <- function(msfit, 
+citab   <- function(msfit,
                     times,
                     timelab = "Time",
                     events = NA,
@@ -31,31 +31,31 @@ citab   <- function(msfit,
                     perc = FALSE,
                     color = "#EEEEEE",
                     printorig = TRUE){
-    
+
     if (printorig) print(summary(msfit, times = times))
-    
+
     est <- summary(msfit, times = times)
-    
+
     probs <- data.frame(est$pstate)
-    lower <- data.frame(est$lower)  
+    lower <- data.frame(est$lower)
     upper <- data.frame(est$upper)
-    
+
     lower[probs == 0 | probs == 1] <- NA
     upper[probs == 0 | probs == 1] <- NA
-    
+
     if (perc){
         probs <- 100*probs
         lower <- 100*lower
         upper <- 100*upper
     }
-    
+
     format <- paste("%.", ci.dec, "f", sep="")
-    
+
     ci <- matrix(rep(NA, nrow(probs)*ncol(probs)), nrow = nrow(probs))
-    
+
     for (i in 1:length(est$states)){
-        ci[,i] <- paste(sprintf(format, round(probs[,i], ci.dec)), 
-                        " [", 
+        ci[,i] <- paste(sprintf(format, round(probs[,i], ci.dec)),
+                        " [",
                         sprintf(format, round(lower[,i], ci.dec)),
                         ", ",
                         sprintf(format, round(upper[,i], ci.dec)),
@@ -64,12 +64,12 @@ citab   <- function(msfit,
     }
     ci <- data.frame(ci)
     names(ci) <- c(est$states[1:(length(est$states)-1)], ifelse(!is.na(state0), state0, "state0"))
-    
+
     ### check if msfit object has stratafication or not
     ### if stratified check group names and apply group labels if any
     grouped <- FALSE
     if (!is.null(msfit$strata)) grouped <- TRUE
-    
+
     ### relabel/reorder events?
     evnts <- names(ci)
     if (!is.na(events[1])) {
@@ -77,16 +77,16 @@ citab   <- function(msfit,
     }
     if (is.na(events[1])) events <- evnts
     if (is.na(evlabs[1])) evlabs <- events
-    
+
     if (!grouped){
         res <- data.frame("time" = c(est$time), rbind(ci))
         # names(res)[1] <- timelab
-        
+
         res <- res[,c("time", events)]
-        
+
         restab <- res[,2:ncol(res), drop = F]
         names(restab) <- evlabs
-        
+
         print(htmlTable(restab,
                         n.cgroup = ncol(restab),
                         cgroup = "Cumulative incidence [95% CI]",
@@ -97,43 +97,45 @@ citab   <- function(msfit,
                         rnames = times,
                         col.rgroup=c('none')))
     }
-    
+
     if (grouped){
-        
+
         stratavar <- strsplit(levels(est$strata)[1], '=')[[1]][1]
         groups_eq <- NA
-        
+
         # check group names (if not NA), remove group names if invalid
         if (!is.na(groups[1])) {
             groups_eq = paste(stratavar, groups, sep="=")
             if (sum(groups_eq %in% est$strata) != nlevels(est$strata)) groups <- grlabs <- NA
         }
-        
-        res <- data.frame("strata" = as.character(est$strata), 
+
+        res <- data.frame("strata" = as.character(est$strata),
                           "time" = est$time,
                           rbind(ci),
                           stringsAsFactors = FALSE)
-        
+
         # apply group labels (if not NA)
+        res$order <- 1:nrow(res)
         if (is.na(groups[1])) groups <- groups_eq <- levels(est$strata)
         if (is.na(grlabs[1])) grlabs <- unlist(lapply(groups_eq, function(x) strsplit(x, '=')[[1]][2]))
-        
+
         res$strata <- factor(as.character(res$strata),
                              levels = groups_eq,
                              labels = grlabs)
+        res <- res[order(as.numeric(res$strata), res$order),]
 
         res <- res[,c("time", "strata", events)]
-        
+
         rows <- table(res$strata)
-        
+
         restab <- res[,3:ncol(res), drop = F]
         names(restab) <- evlabs
-        
+
         print(htmlTable(restab,
                         n.cgroup = ncol(restab),
                         cgroup = "Cumulative incidence [95% CI]",
                         header = names(restab),
-                        rowlabel = timelab,  
+                        rowlabel = timelab,
                         rgroup = grlabs,
                         n.rgroup = rows,
                         rnames = res$time,
